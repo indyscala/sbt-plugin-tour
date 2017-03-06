a52debb1129424b01afdcc9d5d159887a96dae16
Brad Fritz <brad@fewerhassles.com>
2017-03-06 16:18:00 -0500

    add some scaladoc to demo sbt-unidoc

---
+++ unidoc/src/main/scala/org/indyscala/sbt_plugin_tour/MyScalaClass.scala
@@ -0,0 +1,18 @@
+package org.indyscala
+package sbt_plugin_tour
+
+/**
+ * This is a Scala class.  It's awesome!
+ */
+class MyScalaClass {
+  /** This method rocks! */
+  def doSomethingInteresting(): Unit = ???
+}
+
+/**
+ * This is a Scala companion object.  Rad!
+ */
+object MyScalaClass {
+  /** Clever name, eh? */
+  val ScalaVal = "whatever"
+}
